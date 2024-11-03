bbbd00f4c4b4af09de4ba16f21f43ecb3ec004d3
rsudul <alwalt93@gmail.com>
2024-11-03 13:04:28 +0100

    Added movement for basic projectile

--- Assets/_Scripts/Combat/Projectiles/BasicProjectile/BasicProjectile.cs
+++ Assets/_Scripts/Combat/Projectiles/BasicProjectile/BasicProjectile.cs
@@ -1,9 +1,24 @@
+using ProjectSC.Combat.Projectiles.BasicProjectile.Movement;
+using ProjectSC.Combat.Projectiles.BasicProjectile.Settings.Movement;
 using ProjectSC.Combat.Projectiles.Common;
+using UnityEngine;
 
 namespace ProjectSC.Combat.Projectiles.BasicProjectile
 {
     public class BasicProjectile : Projectile
     {
+        private BasicProjectileMovementController _movementController = null;
 
+        [SerializeField]
+        private Rigidbody _rigidbody = null;
+
+        [SerializeField]
+        private BasicProjectileMovementSettings _movementSettings = null;
+
+        private void InitializeControllers()
+        {
+            _movementController = new BasicProjectileMovementController();
+            _movementController.Initialize(_movementSettings);
+        }
     }
 }
