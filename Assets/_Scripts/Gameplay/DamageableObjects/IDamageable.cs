7336ce43f13c5988bfa903571976c9002e5e6ea3
rsudul <alwalt93@gmail.com>
2025-04-05 12:12:24 +0200

    Added events to IDamageable

--- Assets/_Scripts/Gameplay/DamageableObjects/IDamageable.cs
+++ Assets/_Scripts/Gameplay/DamageableObjects/IDamageable.cs
@@ -1,7 +1,12 @@
+using System;
+
 namespace Descent.Gameplay.DamageableObjects
 {
     public interface IDamageable
     {
+        event EventHandler OnDamageTaken;
+        event EventHandler OnDied;
+
         void TakeDamage(int damage);
     }
 }
